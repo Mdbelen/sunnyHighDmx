9d61c2c26bd4f9e544140d9a41b2d4c07ecc398d
Stefan Knipp <4386179+Mdbelen@users.noreply.github.com>
2023-05-12 19:08:26 +0200

    initial Stefan WIP

---
+++ sunnyHighDmx.ino
@@ -0,0 +1,231 @@
+#include "Conceptinetics.h"
+#include <SoftwareSerial.h>
+//#include "FastLED.h"
+
+#define DMX_MASTER_CHANNELS 100
+#define RXEN_PIN 2
+DMX_Master dmx_master(DMX_MASTER_CHANNELS, RXEN_PIN);
+
+#define CTS_PIN 9  // Connect this Trinket pin to BLE 'CTS' pin
+#define RX_PIN 10   // Connect this Trinket pin to BLE 'RX' pin
+#define TXO_PIN 11  // Connect this Trinket pin to BLE 'TXO' pin
+
+#define FPS 60 // Animation frames/second (ish)
+
+unsigned long mainLightData[100][4];
+
+SoftwareSerial ser(RX_PIN, TXO_PIN);
+
+//////////////////////FUNCTION FOR DELAYLESS TIMING
+int brightness = 10;
+int dim = 10;
+int fadeAmount = 5;
+
+unsigned step = 5;  // interval at which to blink (milliseconds)
+
+void setup() {
+  pinMode(CTS_PIN, OUTPUT);
+  digitalWrite(CTS_PIN, HIGH);
+  ser.begin(9600);
+  dmx_master.enable();
+  dmx_master.setChannelRange(1, 100, 0);
+
+}
+
+
+
+
+// FILE ledPar.cpp
+
+#define NUM_ROWS 4
+#define NUM_COLS 2
+
+void setRoofSpot(uint8_t col, uint8_t row, uint8_t red, uint8_t grn, uint8_t blu)
+{
+    uint8_t rowAdd = col==0 ? row * 6 : (3-row) * 6;
+    dmx_master.setChannelValue(col * 24 + rowAdd + 1, red);
+    dmx_master.setChannelValue(col * 24 + rowAdd + 2, grn);
+    dmx_master.setChannelValue(col * 24 + rowAdd + 3, blu);
+}
+
+// col = left right, 0 = window, 1 = bar
+void setRoofCol(uint8_t col, uint8_t red, uint8_t grn, uint8_t blu) {
+  for (uint8_t row = 0; row < NUM_ROWS; row++)
+  {
+    setRoofSpot(col, row, red, grn, blu);
+  }
+}
+
+// row = dj to back, 0 = dj, 3 = back
+void setRoofRow(uint8_t row, uint8_t red, uint8_t grn, uint8_t blu) {
+  for (uint8_t col = 0; col < NUM_COLS; col++)
+  {
+    setRoofSpot(col, row, red, grn, blu);
+  }
+}
+
+void setRoof(uint8_t red, uint8_t grn, uint8_t blu) {
+  for (uint8_t col = 0; col < NUM_COLS; col++)
+  {
+    setRoofCol(col, red, grn, blu);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+uint8_t buf[3],          // Enough for RGB parse; expand if using sensors
+  animMode = 1;          // Current animation mode
+uint32_t prevTime = 0L;  // For animation timing
+
+uint8_t redd = 255, green = 0, blue = 0;
+
+void loop(void) {
+  int c;
+  uint32_t t;
+
+  digitalWrite(CTS_PIN, LOW);  // Signal to BLE, OK to send data!
+  for (;;) {
+    t = micros();                              // Current time
+    if ((t - prevTime) >= (1000000L / FPS)) {  // 1/30 sec elapsed?
+      prevTime = t;
+      break;                        // Yes, go update LEDs
+    }                               // otherwise...
+    if ((c = ser.read()) == '!') {  // Received UART app input?
+      while ((c = ser.read()) < 0)
+        ;  // Yes, wait for command byte
+      switch (c) {
+        case 'B':  // Button (Control Pad)
+          if (readAndCheckCRC(255 - '!' - 'B', buf, 2) & (buf[1] == '1')) {
+            buttonPress(buf[0]);  // Handle button-press message
+          }
+          break;
+        case 'C':  // Color Picker
+          if (readAndCheckCRC(255 - '!' - 'C', buf, 3)) {
+            redd = buf[0];
+            green = buf[1];
+            blue = buf[2];
+          }
+          break;
+      }
+    }
+  }
+  digitalWrite(CTS_PIN, HIGH);
+
+  switch (animMode /*sketch*/) {
+    case 0:
+      fullon();
+      break;
+    case 1:
+      pulse();
+      break;
+    case 2:
+      randomizer();
+      break;
+    case 3:
+      lauflicht();
+      break;
+    case 4:
+      strobe();
+      break;
+    case 5:
+      chaseCol();
+      break;
+    case 6:
+      chaseRow();
+      break;
+  }
+}
+
+boolean readAndCheckCRC(uint8_t sum, uint8_t *buf, uint8_t n) {
+  for (int c;;) {
+    while ((c = ser.read()) < 0)
+      ;                           // Wait for next byte
+    if (!n--) return (c == sum);  // If CRC byte, we're done
+    *buf++ = c;                   // Else store in buffer
+    sum -= c;                     // and accumulate sum
+  }
+}
+
+//CHANGED SERIAL TO SER ERROR GONE... DOES IT CHANGE STH ON THE RUNNING SCRIPT?!
+void skipBytes(uint8_t n) {
+  while (n--) {
+    while (ser.read() < 0)
+      ;
+  }
+}
+
+void buttonPress(char c) {
+  static uint8_t fogLvl = 0;
+  switch (c) {
+    case '1':
+      fogLvl = fogLvl==192 ? 255 : (fogLvl==255 ? 0 : fogLvl+64);
+      dmx_master.setChannelValue(64, fogLvl);
+      //animMode = 4;
+      //dim = 255;
+      //step = 1;
+      break;
+    case '2':
+      dmx_master.setChannelValue(64, 0);
+      break;
+    case '3':
+      if (dim > 5) {
+        dim -= 5;
+      }
+      break;
+    case '4':
+      if (dim < 150) {
+        dim += 5;
+      }
+      break;
+    case '5': //arrowLeft
+      if (step < 24) {
+        step++;
+      }
+      break;
+    case '6': //arrowRight
+      if (step > 1) {
+        step--;
+      }
+      break;
+    case '8':  //up
+      animModeUp();
+      break;
+    case '7':  //down
+      animModeDown();
+      break;
+  }
+}
+
+void animModeUp() {
+  setRoof(0, 0, 0);
+  if (animMode < 6) {
+    animMode++;
+  } else {
+    animMode = 0;
+  }
+}
+
+void animModeDown() {
+  setRoof(0, 0, 0);
+  if (animMode > 0) {
+    animMode--;
+  }
+}
---
+++ wipes.ino
@@ -0,0 +1,244 @@
+
+int loop_speed = 1;  //multiplyer for the pause loop funcion, its loop_speed*led_step
+int multiplyer = 1;
+
+int loop_time = 100;  // time a pause takes, this is what loop_speed counts up to
+int loop_step = 0;
+
+int curPixel = 0;  // current active pixel, gets iterated with led_step
+int maxPixel = 24;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// FULLON - all LEDs only, dimmable
+
+void fullon() {
+  dmx_master.setChannelValue(1, dim);
+  dmx_master.setChannelValue(7, dim);
+  dmx_master.setChannelValue(13, dim);
+  dmx_master.setChannelValue(19, dim);
+  dmx_master.setChannelValue(25, dim);
+  dmx_master.setChannelValue(31, dim);
+  dmx_master.setChannelValue(37, dim);
+  dmx_master.setChannelValue(43, dim);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// PULSE - all LEDs on the same time fade up fade down
+void pulse() {
+
+  // dmx_master.setChannelValue(1, 0);
+  // dmx_master.setChannelValue(7, 0);
+  // dmx_master.setChannelValue(13, 0);
+  // dmx_master.setChannelValue(19, 0);
+  // dmx_master.setChannelValue(25, 0);
+  // dmx_master.setChannelValue(31, 0);
+  // dmx_master.setChannelValue(37, 0);
+  // dmx_master.setChannelValue(43, 0);
+
+  if (loop_step < loop_time) {
+    loop_step += step * 10;
+  } else {
+    loop_step = 0;
+
+    brightness = brightness + dim;
+
+    if (brightness <= 1 || brightness >= 150) {
+      dim = -dim;
+    }
+
+
+    dmx_master.setChannelValue(1, brightness);
+    dmx_master.setChannelValue(7, brightness);
+    dmx_master.setChannelValue(13, brightness);
+    dmx_master.setChannelValue(19, brightness);
+    dmx_master.setChannelValue(25, brightness);
+    dmx_master.setChannelValue(31, brightness);
+    dmx_master.setChannelValue(37, brightness);
+    dmx_master.setChannelValue(43, brightness);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// RANDOMIZER - random on/off ledstrips
+void randomizer() {
+}
+
+
+void strobe() {
+
+  if (loop_step < loop_time) {
+    loop_step += step * 10;
+
+    dmx_master.setChannelValue(1, dim);
+    dmx_master.setChannelValue(7, dim);
+    dmx_master.setChannelValue(13, dim);
+    dmx_master.setChannelValue(19, dim);
+    dmx_master.setChannelValue(25, dim);
+    dmx_master.setChannelValue(31, dim);
+    dmx_master.setChannelValue(37, dim);
+    dmx_master.setChannelValue(43, dim);
+  }
+
+  if (loop_step < loop_time) {
+    loop_step += step * loop_speed;
+
+    dimmAll();
+  }
+}
+
+void lauflicht() {
+
+
+    dimmAll();
+
+  if (loop_step < loop_time) {
+    loop_step += step * 10;
+  } else {
+    for (brightness = 0; brightness <= dim; brightness++) {
+      dmx_master.setChannelValue(1, brightness);
+      dmx_master.setChannelValue(43, brightness);
+    }
+    loop_step = 0;
+  }
+
+  if (loop_step < loop_time) {
+    loop_step += step * 10;
+  } else {
+    for (brightness = 0; brightness <= dim; brightness++) {
+      dmx_master.setChannelValue(7, brightness);
+      dmx_master.setChannelValue(37, brightness);
+    }
+    loop_step = 0;
+  }
+
+  if (loop_step < loop_time) {
+    loop_step += step * 10;
+  } else {
+    for (brightness = 0; brightness <= dim; brightness++) {
+      dmx_master.setChannelValue(13, brightness);
+      dmx_master.setChannelValue(31, brightness);
+    }
+    loop_step = 0;
+  }
+
+  if (loop_step < loop_time) {
+    loop_step += step * 10;
+  } else {
+    for (brightness = 0; brightness <= dim; brightness++) {
+      dmx_master.setChannelValue(19, brightness);
+      dmx_master.setChannelValue(25, brightness);
+    }
+    loop_step = 0;
+  }
+}
+
+
+void dimmAll() {
+
+    fadeAmount = dim;
+    brightness = brightness - fadeAmount;
+
+    // reverse the direction of the fading at the ends of the fade:
+    if (brightness <= 0) {
+      fadeAmount = 0;
+    }
+
+    dmx_master.setChannelValue(1, brightness);
+
+    dmx_master.setChannelValue(7, brightness);
+
+    dmx_master.setChannelValue(13, brightness);
+
+    dmx_master.setChannelValue(19, brightness);
+
+    dmx_master.setChannelValue(25, brightness);
+
+    dmx_master.setChannelValue(31, brightness);
+
+    dmx_master.setChannelValue(37, brightness);
+
+    dmx_master.setChannelValue(43, brightness);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// chaseCol - toggle left and right col, fade
+
+void chaseCol() {
+    uint8_t g_slow = 5;
+    static uint8_t step;
+    static unsigned long last;
+    unsigned long now = micros();
+    unsigned long diff = now - last;
+    last = now;
+    if(diff > 200*g_slow) {
+      step++;
+      uint8_t leftInt = 0;
+      uint8_t rightInt = 0;
+      if(step < 64) {
+        leftInt = step * 4;
+      } else if(step < 128) {
+        leftInt = 255 - ((step-64) * 4);
+      } else if(step < 192) {
+        rightInt = (step - 128) * 4;
+      } else {
+        rightInt = 256 - ((step - 192) * 4);
+      }
+      setRoofCol(0, leftInt, leftInt>>1, 0);
+      setRoofCol(1, rightInt, rightInt>>1, 0);
+    }
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// chaseCol - toggle left and right col, fade
+
+void chaseRow() {
+    uint32_t g_slow = 5;
+
+    static uint8_t step;
+    static uint8_t row;
+    static uint8_t brig;
+
+    static unsigned long last;
+    unsigned long now = micros();
+    unsigned long diff = now - last;
+    bool next = diff > (g_slow * 100);
+    if(next)
+      last = now;
+
+    switch (step)
+    {
+    case 0:
+      if(brig>200) {
+        step++;
+        brig = 0;
+      }
+      if(next)
+        brig++;
+        setRoofRow(row, brig, 0, 0);
+      break;
+    case 1:
+      if(next)
+        brig++;//delay only, dont set value
+      if(brig==200)
+        step++;
+      break;
+    case 2:
+      if(brig==0) {
+        step = 0;
+        row = row==3? 0 : row++;
+        return;
+      }
+      if(next)
+        brig--;
+        setRoofRow(row, brig, 0, 0);
+      break;
+    default:
+      break;
+    }
+
+}
+
+
